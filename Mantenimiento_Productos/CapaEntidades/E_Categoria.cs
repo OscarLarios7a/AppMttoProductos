21b35c8900a478cd392c9ddc1e1baba2e9d57dad
Oscar Larios <javier.atoaxaca@gmail.com>
2021-05-11 18:12:59 -0500

    Anexando herramientas del proyecto

--- Mantenimiento_Productos/CapaEntidades/E_Categoria.cs
+++ Mantenimiento_Productos/CapaEntidades/E_Categoria.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace CapaEntidades
 {
-    class E_Categoria
+    public class E_Categoria
     {
+        //Abstraccion
+        private int idCategoria;
+        private string codigoCategoria;
+        private string nombreCategoria;
+        private string descripcionCategoria;
+
+        ///encapsulamiento 
+
+
+        public int IdCategoria { get { return idCategoria;} set{ idCategoria = value; } }
+
+        public string CodigoCategoria {get{return codigoCategoria;} set{codigoCategoria = value;} }
+
+        public string NombreCategoria
+        {
+            get
+            {
+                return nombreCategoria;
+            }
+
+            set
+            {
+                nombreCategoria = value;
+            }
+        }
+
+        public string DescripcionCategoria
+        {
+            get
+            {
+                return descripcionCategoria;
+            }
+
+            set
+            {
+                descripcionCategoria = value;
+            }
+        }
+
+       
+
     }
 }

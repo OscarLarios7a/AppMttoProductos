409e1ddc57feb7c3d913e45f92c83bacafe33c25
Oscar Larios <javier.atoaxaca@gmail.com>
2021-05-11 16:59:49 -0500

    Agregando los metodos de comunicacion en la capa de Negocios en relacionandola con la capa de Datos

---
+++ Mantenimiento_Productos/CapaNegocio/N_Categoria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidades;
+using CapaDatos;
+
+
+namespace CapaNegocio
+{
+    public class N_Categoria
+    {
+        D_Categoria objDato = new D_Categoria();
+
+        public List<E_Categoria>ListandoCategoria(string buscar)
+        {
+            return objDato.ListarCategorias(buscar);
+        }
+        public void insertandoCategoria(E_Categoria categoria)
+        {
+            objDato.insertarCategoria(categoria);
+        }
+
+        public void editandoCategoria(E_Categoria categoria) {
+            objDato.editarCategoria(categoria);
+        }
+
+        public void eliminandoCategoria(E_Categoria categoria)
+        {
+            objDato.eliminarCategoria(categoria);
+        }
+    }
+}

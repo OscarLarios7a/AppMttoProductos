90d0b3061846d4694897779e21a95886c2ccad43
Oscar Larios <javier.atoaxaca@gmail.com>
2021-05-22 15:34:15 -0500

    Se creo en la Capa de Entidades la clase de E_Productos.cs

---
+++ Mantenimiento_Productos/CapaEntidades/E_Productos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    //se crea la clase de E_Productos en publico para su manipulacion de datos 
+    public class E_Productos
+    {
+        private int idProducto;
+        private string codigoProducto;
+        private string nombreProducto;
+        private decimal precioCompra;
+        private decimal precioVenta;
+        private int stockProducto;
+        private int idCategoria;
+        private int idMarca;
+
+
+    }
+}

34f9823e9d85ea063a34d0f19a07fccb08b3ed79
Oscar Larios <javier.atoaxaca@gmail.com>
2021-05-28 13:33:35 -0500

    Se creo la clase de N_Cliente.cs en la Capa de Negocio el cual tiene comunicacion con la Capa de Datos

---
+++ Mantenimiento_Productos/CapaNegocio/N_Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidades;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class N_Cliente
+    {
+        D_Clientes objClientes = new D_Clientes();
+
+        #region D_Cliente/N_Cliente
+        //Metodo de listado de los clientes entre la capa de Datos de D_cliente y la Capa de Negocio N_Cliente
+        public List<E_Clientes> listandoClientes(string cliente)
+        {
+            return objClientes.listarClientes(cliente);
+        }
+        public void insertarCliente(E_Clientes cliente)
+        {
+            objClientes.insertarClientes(cliente);
+        }
+        public void editarCliente(E_Clientes cliente)
+        {
+            objClientes.editarCliente(cliente);
+        }
+        public void eliminarCliente(E_Clientes cliente)
+        {
+            objClientes.eliminarCliente(cliente);
+        }
+        #endregion
+
+    }
+}
